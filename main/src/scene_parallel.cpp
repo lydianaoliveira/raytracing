ade183451a265e44800da81c5f8ca08a0ef80c8b
lydiana <lydianarodrigues@gmail.com>
2025-07-24 21:21:13 -0300

    update num_threads

--- main/src/scene_parallel.cpp
+++ main/src/scene_parallel.cpp
@@ -64,7 +64,7 @@ int main() {
 
     std::vector<std::vector<vec3>> image(ny, std::vector<vec3>(nx));
 
-    omp_set_num_threads(1);
+    omp_set_num_threads(4);
     #pragma omp parallel for schedule(dynamic)
     for (int j = 0; j < ny; j++) {
         // Gerador de números aleatórios por thread
